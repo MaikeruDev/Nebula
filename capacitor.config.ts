b2670a57dc6befff52072ab01962b318776774d6
Maikeru <78266557+MaikeruDev@users.noreply.github.com>
2023-02-04 17:39:17 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Nebula',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
