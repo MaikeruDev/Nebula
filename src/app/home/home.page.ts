f3dc162b9ed76dac407133264ac38ccf5cbaf9ea
Maikeru <78266557+MaikeruDev@users.noreply.github.com>
2023-02-06 18:18:47 +0100

    Load Posts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ViewerModalComponent } from 'ngx-ionic-image-viewer';
 import { ApiService } from '../services/api.service';
 import { AuthService } from '../services/auth.service';
 
@@ -7,20 +9,40 @@ import { AuthService } from '../services/auth.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   posts: any;
   
-  constructor(private api: ApiService, private auth: AuthService) {}
+  constructor(private api: ApiService, private auth: AuthService, private modalController: ModalController) {}
+
+  ngOnInit(): void {
+    this.api.getPosts().subscribe((data: any) => { 
+      console.log(data)
+      this.posts = data
+    })
+  } 
 
   async logout(){
     this.auth.logout()
   }
 
+  async openViewer(src: any) {
+    const modal = await this.modalController.create({
+      component: ViewerModalComponent,
+      componentProps: {
+        src: src
+      },
+      cssClass: 'ion-img-viewer',
+      keyboardClose: true,
+      showBackdrop: true,
+      swipeToClose: true
+    });
+
+    return await modal.present();
+  }
+
   async load(){
-    this.api.getPosts().subscribe((data: any) => { 
-      this.posts = data
-    })
+    
   }
 
 }
