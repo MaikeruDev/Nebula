a08921f9d6671bb2928b21970898725932b77021
Maikeru <78266557+MaikeruDev@users.noreply.github.com>
2023-02-14 02:05:32 +0100

    Die api_url muss jz so blieba btw

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,8 +6,7 @@ import { User } from '../models/user';
 import { StorageService } from './storage.service';
 import { CryptoService } from './crypto.service'; 
 
-const api_url = "http://michael.prietl.com:3100"
-const api_url = "http://localhost:3100"
+const api_url = "http://michael.prietl.com:3100" 
 
 @Injectable({
   providedIn: 'root'
