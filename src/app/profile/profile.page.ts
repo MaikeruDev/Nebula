c54a232a839eb7e212f914afc0e78b1553ec308f
FIEF-nohell <noel.hmnn@gmail.com>
2023-02-15 22:19:47 +0100

    ok lol

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -12,6 +12,7 @@ import { UserService } from '../services/user.service';
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
+  
 })
 export class ProfilePage implements OnInit {
 
