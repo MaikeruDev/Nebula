6cb21a6c4e5d0fb8e0ddc5b18d4f1f345de1f617
FIEF-Nohell <noel.hmnn@gmail.com>
2023-02-16 21:28:58 +0100

    settings visually edited

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -2,6 +2,7 @@ import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-settings',
@@ -10,9 +11,14 @@ import { AuthService } from '../services/auth.service';
 })
 export class SettingsPage implements OnInit {
 
-  constructor(private location: Location, private auth: AuthService, private nav: NavController) { }
+  user: any = [];
+
+  constructor(private location: Location, private auth: AuthService, private nav: NavController, private userService: UserService) { }
 
   ngOnInit() {
+    this.userService.getCurrentUser().subscribe(user => {
+      this.user = user;
+    });
   }
 
   cancelSettings(){ 
