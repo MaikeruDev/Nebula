bb363212439d9b13cc4bbf942f2e28784b656a48
FIEF-Nohell <noel.hmnn@gmail.com>
2023-02-16 22:26:06 +0100

    WIP Change PFP and Banner

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -4,6 +4,7 @@ import { NavController } from '@ionic/angular';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { UserService } from '../services/user.service';
+import { ImageCroppedEvent } from 'ngx-image-cropper';
 
 @Component({
   selector: 'app-settings',
@@ -15,6 +16,8 @@ export class SettingsPage implements OnInit {
   user: any = [];
   newPP: any = "";
   newBanner: any = "";
+  resizedBanner: any = "";
+  resizedPP: any = "";
 
   changeSettingsForm: FormGroup;
 
@@ -41,6 +44,7 @@ export class SettingsPage implements OnInit {
   async ngOnInit() {
     await this.userService.getCurrentUser().subscribe(user => {
       this.user = user;
+      this.resizedPP = user.ProfilePicture;
       try{
         this.changeSettingsForm.setValue({username: user?.Username, handle: user?.Handle, bio: user?.Bio});
       }
@@ -50,7 +54,40 @@ export class SettingsPage implements OnInit {
     });
   }
 
-  
+  pfpChangeEvent(ev: any){
+    const file = ev.target.files[0];
+    const reader = new FileReader();
+    var width;
+    var height;
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      var newPP = new Image();
+      newPP.src = reader.result as string;
+      this.newPP = reader.result;
+      newPP.onload = async() => {
+        this.compressImage(this.newPP, 512, 512).then(compressed => {
+          this.resizedPP = compressed;
+        })
+      }
+    };
+  }
+
+  async compressImage(src: any, newX: any, newY: any) {
+    return new Promise((res, rej) => {
+      const img = new Image();
+      img.src = src;
+      img.onload = () => {
+        const elem = document.createElement('canvas');
+        elem.width = newX;
+        elem.height = newY;
+        const ctx = elem.getContext('2d');
+        ctx?.drawImage(img, 0, 0, newX, newY);
+        const data = ctx?.canvas.toDataURL();
+        res(data);
+      }
+      img.onerror = error => rej(error);
+    })
+  }
 
   cancelSettings(){ 
     this.nav.back();
