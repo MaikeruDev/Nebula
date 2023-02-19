7bfc2f37c4a7f5faeba147e2ee00a76a0827ca2e
Maikeru <78266557+MaikeruDev@users.noreply.github.com>
2023-02-19 02:20:03 +0100

    PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -10,11 +10,17 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedComponent } from './components/components.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [SharedComponent, BrowserModule, IonicModule.forRoot({mode: "md", navAnimation: iosTransitionAnimation, swipeBackEnabled: false}), AppRoutingModule, HttpClientModule, ReactiveFormsModule],
+  imports: [SharedComponent, BrowserModule, IonicModule.forRoot({mode: "md", navAnimation: iosTransitionAnimation, swipeBackEnabled: false}), AppRoutingModule, HttpClientModule, ReactiveFormsModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
