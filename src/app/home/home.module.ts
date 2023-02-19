c6f9ed6f88779a016978783a4cd8f2e681f7cc38
FIEF-nohell <noel.hmnn@gmail.com>
2023-02-19 23:24:11 +0100

    Merged user suggestions into component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,14 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    ComponentsModule
   ],
   declarations: [HomePage]
 })
